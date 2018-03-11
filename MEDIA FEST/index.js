10e4142f0c444e879f39b5c541d68362e6e38901
Cutwow <ianboraks@gmail.com>
2018-03-11 14:09:05 -0600

    now the carosel is 100% fixed

--- MEDIA FEST/index.js
+++ MEDIA FEST/index.js
@@ -1,17 +1,6 @@
 ﻿$(document).ready(function () {
     $('.carousel.carousel-slider').carousel({ fullWidth: false });
-    $('.carousel.carousel-slider').carousel({})
+    $('.carousel.carousel-slider').carousel();
     $(".dropdown-button").dropdown({ hover: true });
     $('.carousel').carousel();
 });
-
-function load() {
-    var x = confirm("You have entered the Media Censorship website.\n\nDesigned by, Ian Boraks");
-    if (r == true) {
-        break;
-    } else {
-        window.alert("Than get OUT!");
-        break;
-    }
-
-}
