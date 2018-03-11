65ae989719a4b3986b6cfa3e4dfabb54eaba88a0
Cutwow475 <31255827+Cutwow@users.noreply.github.com>
2018-03-11 12:42:58 -0600

    index.js
    this didn't work got rid of the last commit

--- MEDIA FEST/index.js
+++ MEDIA FEST/index.js
@@ -1,5 +1,4 @@
 ﻿$(document).ready(function () {
-    window.confirm("You have entered the Media Censorship website.\n\nDesigned by, Ian Boraks");
     $('.carousel.carousel-slider').carousel({ fullWidth: false });
     $('.carousel.carousel-slider').carousel({})
     $(".dropdown-button").dropdown({ hover: true });
