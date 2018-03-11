b9fca958c2dfb13912b80097cb3abd649e1b751f
Cutwow475 <31255827+Cutwow@users.noreply.github.com>
2018-03-11 12:42:03 -0600

    Updated index.js
    Now has a nice little promt whe nentering the website

--- MEDIA FEST/index.js
+++ MEDIA FEST/index.js
@@ -1,4 +1,5 @@
 ﻿$(document).ready(function () {
+    window.confirm("You have entered the Media Censorship website.\n\nDesigned by, Ian Boraks");
     $('.carousel.carousel-slider').carousel({ fullWidth: false });
     $('.carousel.carousel-slider').carousel({})
     $(".dropdown-button").dropdown({ hover: true });
