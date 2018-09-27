901772247c325e9c5f1c1966ff8870f8aa47d645
Ian Boraks <ianboraks@gmail.com>
2018-09-27 14:07:13 -0600

    fix

--- MEDIA FEST/index.js
+++ MEDIA FEST/index.js
@@ -1,9 +1,11 @@
-﻿$(document).ready(function () {
-    $('.carousel.carousel-slider').carousel({ fullWidth: false });
-    $('.carousel.carousel-slider').carousel();
-    $('.dropdown-button').dropdown({ hover: false });
-    $('.carousel').carousel();
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
 });
-$(document).ready(function () {
-    $('.carousel').carousel();
+var instance = M.Carousel.init({
+    fullWidth: true
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
 });
